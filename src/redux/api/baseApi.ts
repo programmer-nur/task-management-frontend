a4f92ffa3b5f91d6fda4104a0e316b16b31e8520
Nur Mohammad <mohammadnur5480@gmail.com>
2023-12-11 18:58:51 +0600

    feat: added redux axios base query

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,8 @@
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: axiosBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+  endpoints: () => ({}),
+});
